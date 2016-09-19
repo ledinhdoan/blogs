92120655db42b00db3bdf06c062040bc8711e8ef
LeDinhDoan <ledinhdoan.0083@gmail.com>
2016-09-19 23:36:25 +0700

    add comments

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   #get 'welcome/index'
   #root 'welcome#index'
   devise_for :users
-  resources :posts
+  resources :posts do
+  	resources :comments
+  end
   root 'posts#index'
-  #get 'welcome#index'
 end
