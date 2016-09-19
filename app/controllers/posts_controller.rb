1cd3bf1e3e6e8fd0e1bc9a324fe919edfa162550
LeDinhDoan <ledinhdoan.0083@gmail.com>
2016-09-19 22:35:20 +0700

    add validation to post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,22 +2,26 @@ class PostsController < ApplicationController
 def index
 	@posts = Post.all.order('created_at DESC')
 end
-
-def new
-end
-
-def create
-	@post = Post.new(post_params)
-	@post.save
-
-	redirect_to @post
-end
-
 def show
 	@post = Post.find(params[:id])
 end
 
+def new
+	@post = Post.new
+end
+
+def create
+		@post = Post.new(post_params)
+		if @post.save
+			redirect_to @post
+		else
+			render 'new'
+		end
+end
+
+
 private
+
 	def post_params
 		params.require(:post).permit(:title, :body)
 	end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   devise_for :users
   resources :posts
   root 'posts#index'
+  #get 'welcome#index'
 end
